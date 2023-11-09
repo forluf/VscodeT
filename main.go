4936719c581482c0ae354659f07c48cc752a94de
forluf <forluf@gmail.com>
2023-11-09 13:09:13 +0600

    some more

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type men struct {
-	name string
-	age  int
+	Name string
+	Age  int
 }
 
 func main() {
 	var a men
-	a = men{name: "Aex", age: 18}
-	fmt.Printf("%+v", a)
+	a = men{Name: "Alex", Age: 18}
+	bytes, err := json.Marshal(a)
+	fmt.Printf("%v", bytes)
+	errorOnExit(err)
+}
+
+func errorOnExit(err error) {
+	if err == nil {
+		fmt.Println()
+		fmt.Println("No errors")
+	} else {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
